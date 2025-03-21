c98f6562a5807140237aaa329e3f3a18be1e1c60
initionn <initionn@gmail.com>
2025-03-21 15:23:13 -0700

    docs: Add comments on tasking

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
+// main container variable created
 const mainContainer = document.querySelector("#mainContainer");
 
+// square variable created, possibly removed when used in for loop
 const square = document.createElement("div");
 square.classList.add("square");
 square.textContent = "Test to see the div";
 
+// append to make sure it works
 console.log(square);
 mainContainer.appendChild(square);
+

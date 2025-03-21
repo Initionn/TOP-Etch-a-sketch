c646719decd652d0add4efaa6a88ee8871178e00
initionn <initionn@gmail.com>
2025-03-21 15:46:06 -0700

    Feat: add button and initial grid function
    currently only creates an initial set of divs to
    start the grid, effectively the first row of the
    grid. Need to add 1 less than the user selection
    as columns.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,27 @@
 // main container variable created
 const mainContainer = document.querySelector("#mainContainer");
+const title = document.querySelector("h1");
 
 // square variable created, possibly removed when used in for loop
 const square = document.createElement("div");
 square.classList.add("square");
-square.textContent = "Test to see the div";
+square.textContent = "Hello there :)";
 
 // append to make sure it works
-console.log(square);
 mainContainer.appendChild(square);
 
+const newGrid = document.createElement("button");
+
+newGrid.textContent = "New Grid?";
+
+newGrid.addEventListener("click", () => {
+    let userSelection = prompt("How many squares would you like your grid to be?")
+    mainContainer.replaceChildren();
+    for (i = 0; i < userSelection; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.className = "square";
+        newSquare.textContent = "hello";
+        mainContainer.appendChild(newSquare);
+    }
+})
+title.appendChild(newGrid);

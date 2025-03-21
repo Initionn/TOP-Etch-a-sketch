34ab96c1a2bcdbd8adb794ab72293328e7b989ed
initionn <initionn@gmail.com>
2025-03-21 16:15:01 -0700

    Feat: Move grid creation inside an if
    This will handle any issues with the input being
    more than 100 or less than 0.

--- javascript.js
+++ javascript.js
@@ -15,13 +15,18 @@ const newGrid = document.createElement("button");
 newGrid.textContent = "New Grid?";
 
 newGrid.addEventListener("click", () => {
-    let userSelection = prompt("How many squares would you like your grid to be?")
+    let userSelection = prompt("How many squares would you like your grid sides to be? The maximum allowed is 100");
     mainContainer.replaceChildren();
-    for (i = 0; i < userSelection; i++) {
-        const newSquare = document.createElement("div");
-        newSquare.className = "square";
-        newSquare.textContent = "hello";
-        mainContainer.appendChild(newSquare);
-    }
+    if (userSelection > 100 || userSelection < 0) {
+        alert("Oops, can't do that many!")
+    } else {
+        for (i = 0; i < userSelection; i++) {
+            const newSquare = document.createElement("div");
+            newSquare.className = "square";
+            newSquare.textContent = "hello";
+            mainContainer.appendChild(newSquare);
+        }
+        }
+
 })
 title.appendChild(newGrid);
